62de4859cb4f6864838dc3fc0fc9dc4daad69daa
Matheus Loch <122698448+Roonyx43@users.noreply.github.com>
2024-07-11 10:53:44 -0300

    Update log.php

--- log.php
+++ log.php
@@ -1,25 +1,27 @@
 <?php
-    session_start(); // Inicie a sessão
+session_start(); // Inicie a sessão
 
-    if(isset($_POST['entrar']) && !empty($_POST['usuario']) && !empty($_POST['senha'])){
+if (isset($_POST['entrar']) && !empty($_POST['usuario']) && !empty($_POST['senha'])) {
+    include_once('config.php');   
+    
+    $usuario = $_POST['usuario'];
+    $senha = $_POST['senha'];
 
-        include_once('config.php');   
-        $id = $_POST['id']; 
-        $usuario = $_POST['usuario'];
-        $senha = $_POST['senha'];
+    // Prepare a consulta SQL
+    $stmt = $conn->prepare("SELECT * FROM usuarios WHERE usuario = ? AND senha = ?");
+    $stmt->bind_param("ss", $usuario, $senha);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-        $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'";
-
-        $result = $conn->query($sql);
-
-        if(mysqli_num_rows($result) < 1){
-            header('Location: index.php');
-            
-        } else {
-            $row = $result->fetch_assoc();
-            $_SESSION['usuario'] = $row['usuario']; // Armazene informações na session
-            $_SESSION['id'] = $row['id'];
-            header('Location: /PaginaInicial/mainpage.php');
-        }
+    if ($result->num_rows < 1) {
+        header('Location: index.php');
+        exit();
+    } else {
+        $row = $result->fetch_assoc();
+        $_SESSION['usuario'] = $row['usuario']; // Armazene informações na sessão
+        $_SESSION['id'] = $row['id'];
+        header('Location: /PaginaInicial/mainpage.php');
+        exit();
     }
+}
 ?>
