f8d4f5f59b49629bed6bbd2e01705ad59350f59a
Matheus Loch <122698448+Roonyx43@users.noreply.github.com>
2024-08-05 10:47:48 -0300

    Update send_email.php

--- Orçamento/send_email.php
+++ Orçamento/send_email.php
@@ -1,4 +1,10 @@
 <?php
+
+ini_set('upload_max_filesize', '50M');
+ini_set('post_max_size', '50M');
+ini_set('max_execution_time', '300');
+ini_set('memory_limit', '256M');
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 require 'vendor/autoload.php';
