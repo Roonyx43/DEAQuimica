b96085f3447d49b7197bf711389a7fd8bb4ce30a
Matheus Loch <122698448+Roonyx43@users.noreply.github.com>
2024-08-06 10:38:48 -0300

    Update mainpage.php

--- PaginaInicial/mainpage.php
+++ PaginaInicial/mainpage.php
@@ -48,6 +48,7 @@
                         <li class="checklist-lavanderia2"><a href="../Checklist-Diluidor/formdil.php" class="checklist-b">Diluidor</a></li>
                     </ul>
                 </div>
+                <!--
                 <div class="checkboxlist-list" style="width: 80%;">
                     <ul>
                         <h2 class="title-checklists">Clientes</h2>
@@ -64,6 +65,7 @@
                         
                     </ul>
                 </div>
+                -->
             </div>
         </div>
     </main>
